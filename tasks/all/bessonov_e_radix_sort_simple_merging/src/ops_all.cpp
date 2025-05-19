38c0199394b28c02441e013b122bd2068f0f120b
nCUXe <egorkabessonov1@gmail.com>
2025-05-19 22:55:57 +0300

    commit 3

--- tasks/all/bessonov_e_radix_sort_simple_merging/src/ops_all.cpp
+++ tasks/all/bessonov_e_radix_sort_simple_merging/src/ops_all.cpp
@@ -217,7 +217,7 @@ void TestTaskALL::HandleParallelProcess() {
     output_.resize(n);
   }
 
-  MPI_Gatherv(local_sorted.data(), static_cast<int>(local_n), MPI_DOUBLE, output_.data(), sendcounts.data(), 
+  MPI_Gatherv(local_sorted.data(), static_cast<int>(local_n), MPI_DOUBLE, output_.data(), sendcounts.data(),
               displs.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
   if (rank == 0 && size > 1) {
