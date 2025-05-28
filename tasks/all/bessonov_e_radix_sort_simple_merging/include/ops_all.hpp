d5caea8cb7dc2e4a4bd47e1ca987dba58d0d7c42
nCUXe <egorkabessonov1@gmail.com>
2025-05-28 12:29:04 +0300

    all fix 1

--- tasks/all/bessonov_e_radix_sort_simple_merging/include/ops_all.hpp
+++ tasks/all/bessonov_e_radix_sort_simple_merging/include/ops_all.hpp
@@ -21,19 +21,19 @@ class TestTaskALL : public ppc::core::Task {
   bool RunImpl() override;
   bool PostProcessingImpl() override;
 
- private:
-  std::vector<double> input_, output_;
-  boost::mpi::communicator world_;
-
-  void HandleSingleProcess();
-  void HandleParallelProcess();
-  static void MergeChunks(std::deque<std::vector<double>>& chunks);
-
   static void ConvertDoubleToBits(const std::vector<double>& input, std::vector<uint64_t>& bits, size_t start,
                                   size_t end);
   static void ConvertBitsToDouble(const std::vector<uint64_t>& bits, std::vector<double>& output, size_t start,
                                   size_t end);
   static void RadixSortPass(std::vector<uint64_t>& bits, std::vector<uint64_t>& temp, int shift);
   static std::vector<double> Merge(const std::vector<double>& left, const std::vector<double>& right);
+  static void MergeChunks(std::deque<std::vector<double>>& chunks);
+
+ private:
+  std::vector<double> input_, output_;
+  boost::mpi::communicator world_;
+
+  void HandleSingleProcess();
+  void HandleParallelProcess();
 };
 }  // namespace bessonov_e_radix_sort_simple_merging_all
